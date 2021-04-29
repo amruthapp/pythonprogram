87c40d8ddb4f55e224e8fbfab002195667487942
amruthapp <79830617+amruthapp@users.noreply.github.com>
2021-04-29 21:40:12 +0530

    Update program17.php

--- co3/program17.php
+++ co3/program17.php
@@ -21,3 +21,61 @@ echo var_dump($val6)."<br>";
  ?>
 </body>
 </html>
+
+Substr.php
+<html>
+<head>
+<title> String Function: Substr():3 </title>
+</head>
+<body>
+ <?php
+echo substr("Bipin R. Prajapati",0,1)."<br/>";
+echo substr("Bipin R. Prajapati",1,3)."<br/>";
+echo substr("Bipin R. Prajapati",5,2)."<br/>";
+echo substr("Bipin R. Prajapati",8,9)."<br/>";
+echo substr("Bipin R. Prajapati",-3,3)."<br/>";
+echo substr("Bipin R. Prajapati",-9,-3)."<br/>";
+echo substr("Bipin R. Prajapati",1,-10)."<br/>";
+ ?>
+</body>
+</html>
+
+Strcmp.php
+<html>
+<head> <title> String Function:strcmp() </title> </head>
+<body>
+ <?php
+$str1 = 'a';
+$str2 = 'b';
+echo strcmp($str1, $str2)."<br/>";
+$str3 = 'b';
+$str4 = 'a';
+echo strcmp($str3, $str4)."<br/>";
+$str5 = "Anil";
+$str6 = "anil";
+echo strcmp($str5, $str6)."<br/>";
+ ?>
+</body></html>
+
+Strpos.php
+<html>
+<head>
+<title> String Function:strpos():2 </title>
+</head>
+<body>
+ <?php
+$string = "I am anil";
+$pos = strpos($string,"i");
+ if($pos === false)
+ {
+echo "Not found.";
+ }
+ else
+ {
+echo "Found at $pos. .!";
+ }
+ ?>
+</body> 
+</html>
+
+
